710d0a1782243231786d62c6217420dde4516972
LamTonNon <tronglam2403@gmail.com>
2023-01-14 09:24:10 +0700

    Update firmware for Final Project Signed-off-by: LamTonNon <tronglam2403@gmail.com>

--- Marlin/Marlin/Configuration.h
+++ Marlin/Marlin/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 76.1904, 79.20792, 1600, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.3832, 79.3832, 1600, 79 }
 
 /**
  * Default Max Feed Rate (mm/s)
